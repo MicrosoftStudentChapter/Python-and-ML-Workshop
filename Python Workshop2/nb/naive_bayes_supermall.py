d26378589ac1d3d0c8744bc0e91a12e4391daa78
Kulpreet-Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2020-02-08 09:55:46 +0530

    Yash's repo

---
+++ Python Workshop2/nb/naive_bayes_supermall.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Naive Bayes Classifier Machine learning algorithm with example
+@author: SHASHANK
+"""
+
+# Importing the libraries
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
+
+class Model:
+    X = None
+    Y = None
+    standardScaler = None
+   
+    # Importing the dataset
+    def importData(self):
+        dataset = pd.read_csv('supermall.csv')
+        self.X = dataset.iloc[:, [2,3]].values
+        self.Y = dataset.iloc[:, 4].values
+
+    # Applying feature scaling on the train data
+    def doFatureScaling(self):
+        self.standardScaler = StandardScaler()
+        self.X = self.standardScaler.fit_transform(self.X)
+
+    def isBuying(self):
+        self.importData()
+        self.doFatureScaling()
+        
+        # Fitting Naive Bayes algorithm to the Training set
+        classifier = GaussianNB()
+        
+        classifier.fit(self.X, self.Y)    
+        
+        userAge = float(input("Enter the user's age? "))
+        userSalary = float(input("What is the salary of user? "))
+        
+        # Applying feature scaling on the test data
+        testData = self.standardScaler.transform([[userAge, userSalary]])    
+        prediction = classifier.predict(testData)
+        
+      
+        if  (prediction[0]==1):
+            print ('This user is most likely to buy the product')
+        else:
+            print ('This user is not gonna buy the your product.')
+            
+        
+        
+model = Model()
+model.isBuying()
